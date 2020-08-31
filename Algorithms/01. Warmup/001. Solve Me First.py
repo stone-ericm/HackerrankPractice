87f79979ff509bb739632a5a64e9d52bc5f8c96e
Eric Stone <stone.ericm@gmail.com>
2020-08-30 20:55:58 -0400

    Finished Algorithms 1/1

--- Algorithms/01. Warmup/001. Solve Me First.py
+++ Algorithms/01. Warmup/001. Solve Me First.py
@@ -1,2 +1,12 @@
 # Problem: https://www.hackerrank.com/challenges/solve-me-first/problem
 # Score: 1
+
+
+def solveMeFirst(a, b):
+    return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1, num2)
+print(res)
