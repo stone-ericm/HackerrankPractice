ec052e7447391e40d1919cf0b641ff5023da3da3
Eric Stone <stone.ericm@gmail.com>
2020-08-30 21:12:23 -0400

    Finished Algorithms 1/3

--- Algorithms/01. Warmup/003. Compare the Triplets.py
+++ Algorithms/01. Warmup/003. Compare the Triplets.py
@@ -1,2 +1,37 @@
 # Problem: https://www.hackerrank.com/challenges/compare-the-triplets/problem
 # Score: 10
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+
+    result = [0, 0]
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            result[0] += 1
+        elif b[i] > a[i]:
+            result[1] += 1
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
